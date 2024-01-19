66bf76e6b9063bc2dc189eb124dd07ce2aed190d
jrshields86 <jrshields86@gmail.com>
2024-01-18 19:36:05 -0700

    second commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,38 @@
-console.log('hello world')
+const pg = require('pg');
+const client = new pg.Client('postgres://localhost/joe_vacations_db');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const init = async() => {
+    await client.connect();
+    console.log('connected to database');
+    const SQL = `
+        DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS places;
+        DROP TABLE IF EXISTS vacations;
+        CREATE TABLE users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50)
+        );
+        CREATE TABLE places(
+            id SERIAL PRIMARY KEY,
+            place VARCHAR(50)
+        );
+        CREATE TABLE vacations(
+            id SERIAL PRIMARY KEY,
+            place_id INTEGER REFERENCES places(id) NOT NULL,
+            user_id INTEGER REFERENCES users(id) NOT NULL,
+            created_at TIMESTAMP DEFAULT now(),
+            note VARCHAR(100) NOT NULL
+        );
+    `;
+    await client.query(SQL);
+    console.log('create your tables and seed your data');
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+};
+init();
